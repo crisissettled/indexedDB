20cbcd766a09133371f4936e3ae0bb1e794efe11
James Liu <jliu@sirokygroup.com>
2023-11-11 11:36:38 -0500

    version

--- js/index.js
+++ js/index.js
@@ -1,44 +1,62 @@
 const DB_VERSION = {
-  current: 1,
+  current: 2,
   v1: 1,
   v2: 2,
 };
 const DB_NAME = "chat";
-const TABLE_MESSAGE = "message";
+const STORE_MESSAGE = "message";
 
-let openRequest = indexedDB.open(DB_NAME, DB_VERSION.current);
+let dbOpenRequest = indexedDB.open(DB_NAME, DB_VERSION.current);
 
-openRequest.onupgradeneeded = function (event) {
-  let db = openRequest.result;
+dbOpenRequest.onupgradeneeded = function (event) {
+  let db = dbOpenRequest.result;
+  console.log(event.oldVersion, "old version");
+  if (event.oldVersion === 0) {
+    //init schema
+    if (!db.objectStoreNames.contains(STORE_MESSAGE)) {
+      db.createObjectStore(STORE_MESSAGE, {
+        keyPath: "id",
+      });
+    }
+  } else {
+    if (db.objectStoreNames.contains(STORE_MESSAGE)) {
+      db.deleteObjectStore(STORE_MESSAGE);
+    }
+  }
 
-  switch (event.oldVersion) {
-    case 0:
-      //init schema
-      if (!db.objectStoreNames.contains(TABLE_MESSAGE)) {
-        db.createObjectStore(TABLE_MESSAGE, { keyPath: "id" });
-      }
-
-      break;
-    case DB_VERSION.v1:
-      //update schema
-      break;
-    default:
-      console.warn("un-supported version");
+  //update schema based on version
+  if (DB_VERSION.current === DB_VERSION.v2) {
+    if (!db.objectStoreNames.contains(STORE_MESSAGE)) {
+      db.createObjectStore(STORE_MESSAGE, {
+        autoIncrement: true,
+      });
+    }
   }
 };
 
-openRequest.onsuccess = function () {
-  let db = openRequest.result;
-
+dbOpenRequest.onsuccess = function () {
+  let db = dbOpenRequest.result;
+  console.log(db, "db object");
   db.onversionchange = function () {
     db.close();
     alert("Database is outdated, please reload the page.");
   };
 };
 
-openRequest.onerror = function () {
-  let db = openRequest.result;
-  if (db.version !== DB_VERSION.current) {
-    alert("your page is outdated,please reload");
-  }
+dbOpenRequest.onerror = function () {
+  let db = dbOpenRequest;
+  alert(db.error);
 };
+
+document.querySelector("#addMessage").addEventListener("click", function () {
+  let db = dbOpenRequest.result;
+  let transaction = db.transaction(STORE_MESSAGE, "readwrite");
+  let storeMessage = transaction.objectStore(STORE_MESSAGE);
+  let message = {
+    id: 1,
+    price: 10,
+    created: new Date(),
+  };
+
+  storeMessage.add(message);
+});
